6a3fb8acbaa1d5d5ee37746dc50f64ee736353e9
ArthurBava <bavarescospadaarthur@gmail.com>
2024-06-21 15:48:07 -0300

    Atualização

--- main.py
+++ main.py
@@ -20,3 +20,62 @@ pygame.display.set_icon(icone)
 
 # Lista para armazenar os pontos das estrelas
 pontos = []
+
+def jogar():
+    global pontos
+
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont(None, 23)
+    input_text = ""
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                elif event.key == K_RETURN:
+                    print("Informação digitada:", input_text)
+                    input_text = ""
+                elif event.key == K_BACKSPACE:
+                    input_text = input_text[:-1]
+                else:
+                    input_text += event.unicode
+            elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                print("Clique do mouse na posição:", event.pos)
+                nome_stars = simpledialog.askstring("Estrela", "Qual o nome da estrela?")
+                if not nome_stars:
+                    nome_stars = "Desconhecido"
+                cord_mouse = event.pos
+                dados = {
+                    "nome_stars": nome_stars,
+                    "cord_mouse": cord_mouse
+                }
+                pontos.append(dados)
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_F10]:
+            try:
+                with open('dados_space', 'w') as arquivo:
+                    for dados in pontos:
+                        arquivo.write(f"{dados}\n")
+                print("Estrelas salvas com sucesso!")
+            except:
+                print("Erro ao salvar as estrelas!")
+        if keys[pygame.K_F11]:
+            try:
+                with open('dados_space', 'r') as arquivo:
+                    pontos = [eval(dados) for dados in arquivo.readlines()]
+                print("Estrelas carregadas com sucesso!")
+            except:
+                print("Erro ao carregar as estrelas!")
+        if keys[pygame.K_F12]:
+            try:
+                with open('dados_space', 'w') as arquivo:
+                    pass
+                pontos = []
+                print("Estrelas deletadas com sucesso!")
+            except:
+                print("Erro ao deletar as estrelas!")
